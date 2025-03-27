47fa96df22f52c4c3f1f2f3b6df65142fc3191b7
amila-thushantha <thushantha680@gmail.com>
2025-03-27 21:53:44 +0530

    initial commit - backend

---
+++ src/main/java/hr_management_backend/demo/HrManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package hr_management_backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr_management_backend/demo/HrManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr_management_backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
